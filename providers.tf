6b99cb81d854b815b83fa09aece70bffa6c2888d
tsimsek <taha.turan@hotmail.com>
2020-04-13 13:11:15 +0000

    second commit

---
+++ providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+}
+
+data "aws_region" "current" {
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
+}
+
---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
+
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+}
